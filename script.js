7d8dfb13a55ec99fd8e7894927679df4be9c23ec
HolaIamOlya <olyamolchanovaa@icloud.com>
2022-06-14 11:54:17 +0300

    feat: add cv

---
+++ script.js
@@ -0,0 +1,7 @@
+let width = document.documentElement.innertWidth;
+let height = document.documentElement.innerHeight;
+
+let mainDiv = document.getElementById('mainDiv');
+ mainDiv.style.width = width + "px";
+ mainDiv.style.height = height + "px";
+
